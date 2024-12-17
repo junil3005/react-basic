4f2d61b94d3bd19b72c3d0dad0107e603c10e468
junil3005 <yjl3005@naver.com>
2024-12-17 18:29:33 +0900

    summary

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+// import App from './04-create-component/App.jsx';
+import App from "./05-props/App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
